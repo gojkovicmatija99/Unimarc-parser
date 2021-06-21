1232409cd08bbafc6397f0779acf386f731d2c1e
gojkovicmatija99 <gojkovicmatija99@gmail.com>
2021-06-22 00:32:22 +0200

    Parser is working for simple fields

--- parser.hs
+++ parser.hs
@@ -1,12 +1,17 @@
 import Text.Parsec
+import System.Environment
 
 data Field = Field FieldNum Indicators SubFields deriving Show
 newtype FieldNum = FieldNum String deriving Show
 newtype Indicators = Indicators String deriving Show
 newtype SubFields = SubFields [(Char, String)] deriving Show
 
-unimarcParse :: Parsec String () Field
-unimarcParse =  do
+unimarcParse :: Parsec String () [Field]
+unimarcParse = sepEndBy fieldParse $ char '\n'
+                 
+
+fieldParse :: Parsec String () Field
+fieldParse =    do
                 fieldNum <- fieldNumParse
                 spaces
                 indicators <- indicatorsParse
@@ -32,7 +37,7 @@ subfieldsParse = sepEndBy labelContentParse $ char '['
 labelContentParse :: Parsec String () (Char, String)
 labelContentParse = do
                     label <- labelParse
-                    content <- many (noneOf "[")
+                    content <- many (noneOf "[\n")
                     return (label, content)
 
 labelParse :: Parsec String () Char
@@ -40,3 +45,10 @@ labelParse =    do
                 label <- alphaNum
                 char ']'
                 return label
+
+main :: IO ()
+main = do (input:output:[]) <- getArgs
+          cnts <- readFile input
+          case (runParser unimarcParse () input cnts) of  -- parse
+            Left err -> putStrLn . show $ err
+            Right rss -> writeFile output . show $ rss  -- putStrLn . show $ rss
