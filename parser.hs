04d8bc5ecf069cba21cea4da3cf70576234f36da
gojkovicmatija99 <gojkovicmatija99@gmail.com>
2021-06-22 18:18:06 +0200

    Reverting to 1232

--- parser.hs
+++ parser.hs
@@ -1,28 +1,14 @@
 import Text.Parsec
 import System.Environment
 
-newtype Unimarc = Unimarc [Field]
 data Field = Field FieldNum Indicators SubFields deriving Show
 newtype FieldNum = FieldNum String deriving Show
 newtype Indicators = Indicators String deriving Show
-data SubFields = Simple [(Char, String)] | Recurent [(Char, Field)] deriving Show
-
-libraryParse :: Parsec String () [[Field]]
-libraryParse = sepEndBy unimarcParse $ char '\n'
+newtype SubFields = SubFields [(Char, String)] deriving Show
 
 unimarcParse :: Parsec String () [Field]
 unimarcParse = sepEndBy fieldParse $ char '\n'
-
-
--- fieldParse :: Parsec String () Field
--- fieldParse =    do
---                 fieldNum <- fieldNumParse
---                 spaces
---                 indicators <- indicatorsParse
---                 spaces
---                 char '['
---                 subfields <- simpleParse
---                 return $ Field fieldNum indicators $ Simple subfields
+                 
 
 fieldParse :: Parsec String () Field
 fieldParse =    do
@@ -30,8 +16,9 @@ fieldParse =    do
                 spaces
                 indicators <- indicatorsParse
                 spaces
-                subfields <- manyTill recurentLabelContentParse $ oneOf "}"
-                return $ Field fieldNum indicators $ Simple subfields
+                char '['
+                subfields <- subfieldsParse
+                return $ Field fieldNum indicators (SubFields subfields)
 
 fieldNumParse :: Parsec String () FieldNum
 fieldNumParse = do
@@ -44,26 +31,8 @@ indicatorsParse =   do
                     indicator2 <- oneOf "#0123456789"
                     return $ Indicators [indicator1, indicator2]
 
-simpleParse :: Parsec String () [(Char, String)]
-simpleParse = sepEndBy labelContentParse $ char '['
-
--- recurentParse :: Parsec String () Field
--- recurentParse =    do
---                           string "{{"
---                           fieldNum <- fieldNumParse
---                           spaces
---                           indicators <- indicatorsParse
---                           spaces
---                           secondarySubFields <- helperParse
---                           return Field $ fieldNum indicators $ Simple secondarySubFields
-
-recurentLabelContentParse :: Parsec String () (Char, String)
-recurentLabelContentParse =  do
-                          label <- alphaNum 
-                          spaces
-                          content <- manyTill anyChar (string " a ")
-                          return (label, content)
-
+subfieldsParse :: Parsec String () [(Char, String)]
+subfieldsParse = sepEndBy labelContentParse $ char '['
 
 labelContentParse :: Parsec String () (Char, String)
 labelContentParse = do
@@ -80,6 +49,6 @@ labelParse =    do
 main :: IO ()
 main = do (input:output:[]) <- getArgs
           cnts <- readFile input
-          case (runParser libraryParse () input cnts) of
-            Left err -> print err
-            Right library -> writeFile output . show $ library
+          case (runParser unimarcParse () input cnts) of  -- parse
+            Left err -> putStrLn . show $ err
+            Right rss -> writeFile output . show $ rss  -- putStrLn . show $ rss
