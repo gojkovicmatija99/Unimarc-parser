4083961086200d0bcdcd67699e7ed9bf53e33ecd
gojkovicmatija99 <gojkovicmatija99@gmail.com>
2021-07-12 21:21:12 +0200

    Searching completed, working on json conversion

--- parser.hs
+++ parser.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
 import Text.Parsec
 import System.Environment
 
@@ -5,7 +7,7 @@ newtype Unimarc = Unimarc [Field] deriving Show
 data Field = Field FieldNum Indicators SubFields deriving Show
 newtype FieldNum = FieldNum String deriving Show
 newtype Indicators = Indicators String deriving Show
-newtype SubFields = SubFields [(Char, String)] deriving Show
+data SubFields = Simple [(Char, String)] | Recurent [(Char, Field)] deriving Show
 
 libraryParse :: Parsec String () [Unimarc]
 libraryParse = sepEndBy unimarcParse $ char '\n'
@@ -24,7 +26,7 @@ fieldParse =    do
                 spaces
                 char '['
                 subfields <- subfieldsParse
-                return $ Field fieldNum indicators (SubFields subfields)
+                return $ Field fieldNum indicators (Simple subfields)
 
 fieldNumParse :: Parsec String () FieldNum
 fieldNumParse = do
@@ -51,6 +53,76 @@ labelParse =    do
                 label <- alphaNum
                 char ']'
                 return label
+
+------------------------------------------------------------------------------------------
+
+instance Eq FieldNum where
+        FieldNum a == FieldNum b = a == b
+
+searchLibrary:: [Unimarc] -> FieldNum -> Char -> String
+searchLibrary [] fieldNum label = "NOT FOUND"
+searchLibrary (x:xs) fieldNum label = if searchUnimarc x fieldNum label == "NOT FOUND" then searchLibrary xs fieldNum label else searchUnimarc x fieldNum label
+
+searchUnimarc:: Unimarc -> FieldNum -> Char -> String
+searchUnimarc (Unimarc []) _ _ = "NOT FOUND"
+searchUnimarc (Unimarc (x:xs)) fieldNum label = if searchField x fieldNum label == "NOT FOUND" then searchUnimarc (Unimarc xs) fieldNum label else searchField x fieldNum label
+
+searchField:: Field -> FieldNum -> Char -> String
+searchField (Field fieldNum indicators subFields) searchFieldNum label = if fieldNum == searchFieldNum then searchSubFields subFields label else "NOT FOUND"
+
+searchSubFields:: SubFields -> Char -> String
+searchSubFields (Simple []) _ = "NOT FOUND"
+searchSubFields (Simple (x:xs)) label =    if checkChar x label == "NOT FOUND" then searchSubFields (Simple xs) label else checkChar x label
+
+checkChar:: (Char, String) -> Char -> String
+checkChar (char, string) label = if char == label then string else "NOT FOUND"
+
+-----------------------------------------------------------------------------------------
+length' :: (Num b) => [a] -> b 
+length' [] = 0 
+length' xs = sum [1 | _ <- xs]
+
+
+toJsonLibrary:: [Unimarc] -> String
+toJsonLibrary library = "{ library: [" ++ libraryHelper library ++ "]}"
+
+libraryHelper:: [Unimarc] -> String
+libraryHelper [] = ""
+libraryHelper (x:xs)
+        | length' xs > 0 = toJsonUnimarc x ++ "," ++ libraryHelper xs
+        | otherwise = toJsonUnimarc x ++ libraryHelper xs
+
+toJsonUnimarc:: Unimarc -> String
+toJsonUnimarc unimarc = "unimarc: [" ++ unimarcHelper unimarc ++ "]"
+
+unimarcHelper:: Unimarc -> String
+unimarcHelper (Unimarc []) = ""
+unimarcHelper (Unimarc (x:xs))
+        | length' xs > 0 = toJsonField x ++ "," ++ unimarcHelper (Unimarc xs)
+        | otherwise = toJsonField x ++ unimarcHelper (Unimarc xs)
+    
+toJsonField:: Field -> String
+toJsonField (Field fieldNum indicators subFields) = "{" ++ toJsonFieldNum fieldNum ++ "," ++ toJsonIndicators indicators ++ "," ++ toJsonSubFields subFields ++ "}"
+
+toJsonFieldNum::FieldNum -> String
+toJsonFieldNum (FieldNum fieldNum) = "fieldNum: " ++ fieldNum
+
+toJsonIndicators:: Indicators -> String
+toJsonIndicators (Indicators ind) = "indicators: " ++ ind
+
+toJsonSubFields:: SubFields -> String
+toJsonSubFields subFields = "subFields: [" ++ subFieldHelper subFields ++ "]"
+
+subFieldHelper:: SubFields -> String
+subFieldHelper (Simple []) = ""
+subFieldHelper (Simple (x:xs)) 
+        | length' xs > 0 = showSubFields x ++ "," ++ subFieldHelper (Simple xs)
+        | otherwise = showSubFields x ++ subFieldHelper (Simple xs)
+
+showSubFields:: (Char, String) -> String
+showSubFields (label, content) = "{label:" ++ show label ++ ",content:" ++ show content ++ "}"
+
+-----------------------------------------------------------------------------------------
 
 main :: IO ()
 main = do (input:output:[]) <- getArgs
